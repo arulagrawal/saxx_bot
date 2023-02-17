b97496121aed5f05668e05ebb50816f7f5f6b684
Arul Agrawal <me@arul.io>
2023-02-17 15:40:28 +0200

    add basic time collection

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,42 @@
-import { Client, GuildMember, Snowflake, User } from "discord.js";
+import { Client, Events } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
+import { onInteraction } from "./events/newInteraction";
+import { onReady } from "./events/onReady";
+
+interface event {
+  joinTime?: Date
+  leaveTime?: Date
+}
+
+let dates: Record<string, event> = {};
+
 
 (async () => {
   const BOT = new Client({ intents: IntentOptions });
 
-  BOT.on("ready", () => console.log("Connected to Discord!"));
+  BOT.on("ready", async () => await onReady(BOT));
+
+  BOT.on(
+    "interactionCreate",
+    async (interaction) => await onInteraction(interaction)
+  );
 
   BOT.on("voiceStateUpdate", async (oldState, newState) => {
     let newUserChannel = newState.channel;
     let oldUserChannel = oldState.channel;
     if (oldUserChannel === null && newUserChannel !== null) {
-      const user = newState.member?.user?.username;
+      const user = newState.member?.user?.username ?? "wtf";
       console.log(`${user} joined`);
+      const date = new Date();
+      dates[user] = {joinTime: date};
+
     } else if (oldUserChannel !== null && newUserChannel === null) {
-      const user = oldState.member?.user?.username;
+      const user = oldState.member?.user?.username ?? "wtf";
       console.log(`${user} left`);
+      const date = new Date();
+      dates[user].leaveTime = date;
+
+
     } else if (
       oldUserChannel !== null &&
       newUserChannel !== null &&
