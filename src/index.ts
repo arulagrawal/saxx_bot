a6b8e2c853d0dc54a07f877f79cbaa6d2e2eda97
Arul Agrawal <me@arul.io>
2023-02-17 17:17:57 +0200

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
 import { onInteraction } from "./events/newInteraction";
 import { onReady } from "./events/onReady";
+import { voiceUpdate } from "./events/voiceUpdate";
+import { validateEnv } from "./utils/validateEnv";
 
 interface event {
   joinTime?: Date
@@ -10,8 +12,8 @@ interface event {
 
 let dates: Record<string, event> = {};
 
-
 (async () => {
+  if (!validateEnv()) return;
   const BOT = new Client({ intents: IntentOptions });
 
   BOT.on("ready", async () => await onReady(BOT));
@@ -22,26 +24,7 @@ let dates: Record<string, event> = {};
   );
 
   BOT.on("voiceStateUpdate", async (oldState, newState) => {
-    let newUserChannel = newState.channel;
-    let oldUserChannel = oldState.channel;
-    if (oldUserChannel === null && newUserChannel !== null) {
-      const user = newState.member?.user?.username ?? "wtf";
-      console.log(`${user} joined`);
-      const date = new Date();
-      dates[user] = { joinTime: date };
-    } else if (oldUserChannel !== null && newUserChannel === null) {
-      const user = oldState.member?.user?.username ?? "wtf";
-      console.log(`${user} left`);
-      const date = new Date();
-      dates[user].leaveTime = date;
-    } else if (
-      oldUserChannel !== null &&
-      newUserChannel !== null &&
-      oldUserChannel.id != newUserChannel.id
-    ) {
-      const user = newState.member?.user?.username;
-      console.log(`${user} switched channels`)
-    }
+    await voiceUpdate(oldState, newState);
   });
 
 
